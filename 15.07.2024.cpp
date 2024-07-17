a2f54ca9ed56d7505cfca0d882653aa878eb7d70
GriFFoN-Noriaki <zhabadryn@gmail.com>
2024-07-17 15:29:36 +0500

    Add files via upload

---
+++ 15.07.2024.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main() 
+{
+    setlocale(LC_ALL, "rus");
+
+    int a, b;
+
+    // Ввод чисел
+    std::cout << "Введите два числа через пробел: ";
+    std::cin >> a >> b;
+
+    // Вычисление целой части от деления первого числа на второе
+    int divisionResult = a / b;
+    std::cout << "Целая часть от деления числа " << a << " на " << b << ": " << divisionResult << std::endl;
+
+    // Вычисление квадрата меньшего числа
+    int minNumber = (a < b) ? a : b;
+    int square = minNumber * minNumber;
+    std::cout << "Квадрат меньшего из чисел: " << square << std::endl;
+
+    // Вычисление куба большего числа
+    int maxNumber = (a > b) ? a : b;
+    int cube = maxNumber * maxNumber * maxNumber;
+    std::cout << "Куб большего из чисел: " << cube << std::endl;
+
+    return 0;
+}
